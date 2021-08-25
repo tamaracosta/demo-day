2312335b1c17ab626961bf80f14994d1faa8cea9
Lediane <lediane141@gmail.com>
2021-08-25 14:04:40 -0300

    css e html login e register

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -1,1 +1,54 @@
-export const Login = () => {};
+export const Login = () => {
+  const rootElement = document.createElement('div');
+  rootElement.className = 'login-container';
+  rootElement.innerHTML = `
+  <section class="login-left-section">
+  <main>
+    <div class=img-login>
+      <img src="./logo-login.png"/>
+    </div>
+    <form>
+      <div class="div-email" id="div-email">
+        <input required type="email" class="input-email" id="input-email" placeholder=" " autocomplete="off">
+        <label for="input-email" class="login-input-label" id="label-input-email">Email</label>
+      </div>
+
+      <div class="div-password" id="div-password">
+        <input required type="password" class="input-password" id="input-password" placeholder=" " autocomplete="off">
+          <img class="eye" id="eye" src="./eye-off.png"/>
+          <label for="input-password" class="login-input-label">Senha</label> 
+        <div class='checkbox-forgot-password'>
+        <input type="checkbox" id="checkbox-keep-logged-in">
+        <label for="checkbox-keep-logged-in" class="checkbox-keep-logged-in"> Mantenha-me logada</label>
+  
+      <button id="btn-forgot-password"> Esqueceu a senha?</button>
+    </div>
+      </div>
+      
+    </form>
+    <div class="login-error-div" id="print-error-here"> 
+      <p class="transparent"> . </p>
+    </div>
+    <div class="login-btn-sig-log">
+      <button id="btn-login" class="btn-login">ENTRAR</button>  
+    </div>
+    <div class="register-new-user">
+        Não possui conta? <button id="btn-sign-in">Registre-se</button>
+    </div>
+    <p class="connect">Ou conecte-se com</p>
+    <div class="enter-icons">
+        <button id="btn-login-with-google"><img src="./google-btn.png"> </button>
+        <button id="btn-login-with-insta"> <img src="./insta-btn.jpeg"></button>
+        <button id='btn-login-with-facebook'><img src="./face-btn.jpeg"></button>
+    </div>
+  </main>
+  <footer> Not Alone &#169;</footer>
+</section> 
+    `;
+};
+
+/* <div class="login-additionals">
+      <button id="btn-login-with-google"> Ou conecte-se com o gmail</button>
+      <br>
+      <button id="btn-forgot-password"> Esqueceu a senha?</button>
+    </div> */
--- src/pages/register/main.js
+++ src/pages/register/main.js
@@ -1,1 +1,47 @@
-export const Register = () => {};
+export const Register = () => {
+  const rootElement = document.createElement('div');
+  rootElement.classList.add('register-container');
+  rootElement.innerHTML = `
+    <main>
+      <div class='img-login'>
+        <img class="img-logo" src="./logo-login.png">
+      </div>
+      <form>
+          <section class="content">
+        <div class="div-name">
+          <input required type="text" class='input-name'id="input-name" placeholder=" " autocomplete="off">
+          <label for="input-name"class="login-input-label">Nome</label>
+        </div>
+        <div class="div-email">
+          <input required type="email" class="input-email" id="input-email" placeholder=" " autocomplete="off">
+          <label for="input-email" class="login-input-label" id="label-input-email">Email</label>
+        </div>
+
+        <div class="div-password" class='div-password'>
+          <input required type="password" class='input-password'id="input-password" placeholder=" " autocomplete="off">
+          <img class="icon-eye" id="icon-eye" src="./eye-off.png"/>
+          <label class="login-input-label">Senha</label> 
+        </div>
+
+        <div class="div-confirm-password">
+            <input required type="password" class='input-confirm-password' id="input-confirm-password" placeholder=" " autocomplete="off">
+            <img class="icon-eye" id="icon-eye" src="./eye-off.png"/>
+            <label class="login-input-label">Confirme a senha</label> 
+          </div>
+          </section>
+
+          <div class="checkbox-div">
+        <input type="checkbox" class="checkbox-keep-logged-in" id="checkbox-keep-logged-in">
+        <label for="checkbox-keep-logged-in" class="checkbox-keep"> Mantenha-me logada </label>
+          </div>
+      </form>
+      <div id="print-error-here"> 
+        <p class="transparent"> . </p>
+      </div>
+      <div class="register-btn-register">
+        <button id="btn-register-account">REGISTRAR</button>
+      </div>
+    </main>
+    <footer class="register-footer"> Not Alone ©</footer>
+    `;
+};
