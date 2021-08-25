da91731755e095c5c03443693788fe7ea0371c4e
Lais Mune <aymune@hotmail.com>
2021-08-25 14:16:31 -0300

    estruturacao feed

--- src/pages/feed/main.js
+++ src/pages/feed/main.js
@@ -1,3 +1,11 @@
+import {
+  getPosts, deletePost,
+} from '../../lib/firebase-services.js';
+
+import {
+  updateLikes, getComments, getCurrentCommentsToPrint,
+} from './index.js';
+
 export const Feed = () => {
   const rootElement = document.createElement('div');
   rootElement.className = 'feed-container';
@@ -16,7 +24,6 @@ export const Feed = () => {
   <main>
     <form class='feed-publication-form'>
       <textarea class='feed-publication-text-area' id='publication-text-area' placeholder='O que você quer publicar hoje?'></textarea> 
-      <input class='feed-hide-url-in-text-area' id='hide-url-in-text-area> </input>
       <section class='feed-publication-buttons-area'>
         <button class='btn feed-publication-image-btn' id='publication-image-btn'></button>
         <form method="post">
@@ -30,25 +37,52 @@ export const Feed = () => {
   <footer><footer>
   `;
 
+  const postsCollection = firebase.firestore().collection('posts');
+  const currentUserEmail = firebase.auth().currentUser.email;
+
   function createPostTemplate(post) {
     const postTemplate = `
-    <section class='feed-post-owner-data'>
-      <img class='feed-post-owner-picture'>
-      <span class='feed-post-owner-name'>
-      <span class='feed-post-data'>
-    </section>
-    <section class='feed-post-content-section'>
-      <p class='feed-post-content'></p>
-      <img class='feed-post-image'> </img>
-    </section>
-    <section class='feed-post-actions-section'>
-      <div class='feed-post-actions-left-section'>
-        <button> Like </button>
-        <button> Comment </button>
-      </div>
-    <div class='feed-post-actions-right-section'>
-      <button> Edit</button>
-      <button> Delete </button>
+    <div class="feed-all-the-post" data-postId="${post.id}" data-postOwner="${post.data().user_id}">
+      <section class='feed-post-owner-data'>
+        <img class='feed-post-owner-picture'>
+        <span class='feed-post-owner-name'> ${post.data().user_id} em: </span>
+        <span class='feed-post-data'> ${post.data().data} </span>
+      </section>
+      <section class='feed-post-content-section'>
+        <p class='feed-post-content'> ${post.data().text} </p>
+        <img class='feed-post-image'> </img>
+      </section>
+      <section class='feed-post-actions-section'>
+        <div class='feed-post-actions-left-section'>
+      ${((likes) => {
+    if (likes.length > 0) {
+      if (likes.includes(currentUserEmail)) {
+        return `<button class='btn full-like-btn' data-likePostButton='${post.id}'></button> `;
+      } return `<button class='btn empty-like-btn' data-likePostButton='${post.id}'></button> `;
+    } return `<button class='btn empty-like-btn' data-likePostButton='${post.id}'></button> `;
+  })(post.data().likes)}
+          <span data-likeValueToChange='${post.id}'> ${post.data().likes.length} </span>
+          <button class='btn comment-btn' data-showCommentPostButton='${post.id}'></button>
+        </div>
+        <div class='feed-post-actions-right-section'>
+      ${((user) => {
+    if (user === currentUserEmail) {
+      return `<button class='btn edit-btn' data-editPostButton='${post.id}'></button>
+              <button class='btn delete-btn' data-deletePostButton='${post.id}'></button>`;
+    } return `<button class='not-allowed-to-see'></button>
+              <button class='not-allowed-to-see'></button>`;
+  })(post.data().user_id)}
+        </div>
+      </section>
+      <section class='feed-comments-section' data-commentsSection='${post.id}'>
+        <div class='feed-comment-input'>
+          <textarea class='feed-comment-text-area' data-commentContent='${post.id}' placeholder='Digite seu comentário aqui:'></textarea> 
+          <button class='btn feed-comment-btn' data-commentPostButton='${post.id}'></button>
+        </div>
+        <div class='feed-printed-comments'>
+          <ul data-ulCommentArea='${post.id}'> </ul>
+        </div>
+      </section>
     </div>
     `;
     return postTemplate;
@@ -61,17 +95,19 @@ export const Feed = () => {
     chooseAnImageBtn.style.visibility = 'visible';
   });
 
+  // Post creation section:
   function createAndPrintAllPosts(post) {
     const postElement = document.createElement('div');
     postElement.id = post.id;
     postElement.classList.add('feed-a-post');
-    rootElement.querySelector('#hide-url-in-text-area').value = '';
+    // rootElement.querySelector('#hide-url-in-text-area').value = '';
     const postTemplate = createPostTemplate(post);
     postElement.innerHTML = postTemplate;
     rootElement.querySelector('#posts-section').appendChild(postElement);
   }
 
   function loadPosts() {
+    rootElement.querySelector('#posts-section').innerHTML = '';
     getPosts(createAndPrintAllPosts);
   }
 
@@ -84,26 +120,75 @@ export const Feed = () => {
     event.preventDefault();
     const textArea = rootElement.querySelector('#publication-text-area');
     const postContent = rootElement.querySelector('#publication-text-area').value;
-    const postImageUrl = rootElement.querySelector('#hide-url-in-text-area').value;
+    // const postImageUrl = rootElement.querySelector('#hide-url-in-text-area').value;
 
     const post = {
       text: postContent,
-      url: postImageUrl,
+      // url: postImageUrl,
       user_id: currentUserEmail,
       data: postData(),
       likes: [],
       comments: [],
     };
 
-    if (postContent.value === '') {
+    if (textArea.value === '') {
       return;
     }
     postsCollection.add(post).then(() => {
-      rootElement.querySelector('#postContent').value = '';
+      rootElement.querySelector('#publication-text-area').value = '';
       rootElement.querySelector('#posts-section').innerHTML = '';
       loadPosts();
     });
   });
 
+  // Comment creation section:
+  const printComments = (commentsToPrint, postID) => {
+    const commentArea = rootElement.querySelector(`[data-ulCommentArea="${postID}"]`);
+    commentArea.innerHTML = '';
+    commentsToPrint.forEach((comment) => {
+      const newItem = `
+      <li class="comment-f-20" id="${comment.id}"> </li>
+      `;
+      commentArea.innerHTML += newItem;
+    });
+  };
+
+  // Action Buttons:
+  const postsSection = rootElement.querySelector('#posts-section');
+  postsSection.addEventListener('click', (e) => {
+    const { target } = e;
+    const postID = target.parentNode.parentNode.parentNode.parentNode.id;
+
+    // Delete Post:
+    const deletePostBtn = target.dataset.deletepostbutton;
+    if (deletePostBtn) {
+      deletePost(postID, loadPosts);
+    }
+
+    // Like Post:
+    const likePostBtn = target.dataset.likepostbutton;
+    if (likePostBtn) {
+      const valueToBeChanged = rootElement.querySelector(`[data-likeValueToChange="${postID}"]`);
+      const amountOfLikes = parseInt(valueToBeChanged.textContent, 10);
+      const likeStatus = rootElement.querySelector(`[data-likePostButton="${postID}"]`);
+      updateLikes(postID, currentUserEmail, valueToBeChanged, amountOfLikes, likeStatus);
+    }
+
+    // Show Comments Section:
+    const showCommentPostBtn = target.dataset.showcommentpostbutton;
+    if (showCommentPostBtn) {
+      const commentsSection = rootElement.querySelector(`[data-commentsSection="${postID}"]`);
+      commentsSection.style.display = 'flex';
+    }
+
+    // Comment Post:
+    const commentPostBtn = target.dataset.commentpostbutton;
+    if (commentPostBtn) {
+      const newCommentContent = rootElement.querySelector(`[data-commentContent="${postID}"]`).value;
+      getCurrentCommentsToPrint(postID, newCommentContent, currentUserEmail, printComments);
+    }
+  });
+
+  loadPosts();
   return rootElement;
 };
--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -1,1 +1,74 @@
-export const Login = () => {};
+import {
+  loginWithGoogle,
+} from '../../lib/firebase-services.js';
+
+export const Login = () => {
+  const rootElement = document.createElement('div');
+  rootElement.className = 'login-container';
+  rootElement.innerHTML = `
+  <section class="login-left-section">
+    <header>
+      <a>Sobre nós</a>
+    </header>
+    <main>
+      <div>
+        <img src="./images/logo-login.png"/>
+      </div>
+      <form>
+        <div>
+          <input required type="email" id="input-email">
+          <label class="login-input-label" id="label-input-email">Email</label>
+        </div>
+        <div class="div-password" id="div-password">
+          <input required type="password" class="input-password" id="input-password">
+            <img class="eye" id="eye" src="./images/eye-off.png"/>
+            <label class="login-input-label">Senha</label> 
+          <br>
+          <label class="checkbox-keep-logged-in"> Mantenha-me logadex
+            <input type="checkbox" id="checkbox-keep-logged-in">
+          </label>
+        </div>
+      </form>
+      <div class="login-error-div" id="print-error-here"> 
+        <p class="transparent"> . </p>
+      </div>
+      <div class="login-btn-sig-log">
+        <button id="btn-login">ENTRAR</button>  
+        <button id="btn-sign-in">CADASTRAR</button>
+      </div>
+      <div class="login-additionals">
+        <button id="btn-login-with-google"> Ou conecte-se com o gmail</button>
+        <br>
+        <button id="btn-forgot-password"> Esqueceu a senha?</button>
+      </div>
+    </main>
+    <footer> Willing &copy 2021</footer>
+  </section> 
+  `;
+
+  const getUserEmail = rootElement.querySelector('#input-email');
+  const getUserPassword = rootElement.querySelector('#input-password');
+  const checkboxKeepLoggedIn = rootElement.querySelector('#checkbox-keep-logged-in');
+
+  getUserEmail.addEventListener('keyup', (event) => {
+      const labelEmail = rootElement.querySelector('#label-input-email');
+      const inputEmailValue = event.target.value;
+      if (inputEmailValue.length > 1) {
+          labelEmail.classList.add('login-input-label-up');
+      }
+  });
+
+
+
+  const btnLoginWithGoogle = rootElement.querySelector('#btn-login-with-google');
+  btnLoginWithGoogle.addEventListener('click', () => {
+      loginWithGoogle(checkboxKeepLoggedIn);
+  });
+
+
+ 
+
+
+
+  return rootElement;
+};
