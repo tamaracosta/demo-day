59f10327cf9e6e0acd729e4e91ace71eb9481f09
tamaracosta <tamarasantosc@gmail.com>
2021-08-26 23:21:19 -0300

    horas, foto, editar e salvar publi personal feed

--- src/pages/personal-feed/main.js
+++ src/pages/personal-feed/main.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
-import { deletePost, getMyPosts } from '../../lib/firebase-services.js';
+import { deletePost, getMyPosts, editPost } from '../../lib/firebase-services.js';
 import {
-  updateLikes, getComments, getCurrentCommentsToPrint,
+  updateLikes, getComments, getCurrentCommentsToPrint, publicationAge
 } from '../feed/index.js';
 
 export const PersonalFeed = () => {
@@ -40,9 +40,6 @@ export const PersonalFeed = () => {
     </header>
       
   </main>
-
-
-
   
   <div class="popup-wrapper">
       <div class="popup">
@@ -58,16 +55,26 @@ export const PersonalFeed = () => {
   const currentUserEmail = firebase.auth().currentUser.email;
 
   function createPostTemplate(post) {
+    const currentDate = Date.now();
+    const timeInSeconds = ((currentDate - post.data().creationDate) / 1000);
+    const postAge = publicationAge(timeInSeconds);
+
     const postTemplate = `
         <div class="feed-all-the-post" data-postId="${post.id}" data-postOwner="${post.data().user_id}">
           <section class='feed-post-owner-data'>
-            <img class='feed-post-owner-picture'>
-            <span class='feed-post-owner-name'> ${post.data().user_id} em: </span>
-            <span class='feed-post-data'> ${post.data().data} </span>
+            <img class='feed-post-owner-picture' src="${firebase.auth().currentUser.photoURL}" onerror="this.src='../images/avatar2.png'; this.onerror=null" />
+            <span class='feed-post-owner-name'> ${firebase.auth().currentUser.displayName || post.data().user_id} em: </span>
+            <span class='feed-post-data'> ${postAge} </span>
           </section>
           <section class='feed-post-content-section'>
-            <p class='feed-post-content' contenteditable="false"> ${post.data().text} </p>
-            <img src="../images/prato.png" class='feed-post-image'/>
+            <p class='feed-post-content' data-feedPostContent='${post.id}'> ${post.data().text} </p>
+            <textarea class='feed-edit-text-area' data-editTextArea='${post.id}'>${post.data().text}</textarea>          
+            <img class='feed-post-image'> </img>
+            ${((url) => {
+              if (url !== '') {
+                return `<img class='feed-posted-image' src="${post.data().url}"> </img>`;
+              } return `<img id="hide-img" src="${post.data().url}"> </img>`;
+            })(post.data().url)}
           </section>
           <section class='feed-post-actions-section'>
             <div class='feed-post-actions-left-section'>
@@ -82,6 +89,7 @@ export const PersonalFeed = () => {
               <button class='btn comment-btn' data-showCommentPostButton='${post.id}'></button>
             </div>
             <div class='feed-post-actions-right-section'>
+            <button class='btn save-edit-btn' data-saveEditPostButton='${post.id}'></button>
           ${((user) => {
         if (user === currentUserEmail) {
           return `<button class='btn edit-btn' data-editPostButton='${post.id}'></button>
@@ -96,11 +104,13 @@ export const PersonalFeed = () => {
               <textarea class='feed-comment-text-area' data-commentContent='${post.id}' placeholder='Digite seu comentário aqui:'></textarea> 
               <button class='btn feed-comment-btn' data-commentPostButton='${post.id}'></button>
             </div>
+
             <div class='feed-printed-comments'>
               <ul data-ulCommentArea='${post.id}'> </ul>
             </div>
           </section>
         </div>
+        <hr class='feed-post-end-line'>
         `;
     return postTemplate;
   }
@@ -109,7 +119,6 @@ export const PersonalFeed = () => {
     const postElement = document.createElement('div');
     postElement.id = post.id;
     postElement.classList.add('feed-a-post');
-
     const postTemplate = createPostTemplate(post);
     postElement.innerHTML = postTemplate;
     rootElement.querySelector('#posts-section').appendChild(postElement);
@@ -121,12 +130,7 @@ export const PersonalFeed = () => {
     const { target } = e;
     const postID = target.parentNode.parentNode.parentNode.parentNode.id;
 
-    // Editar Post
-    const editPostBtn = target.dataset.editpostbutton;
-    if (editPostBtn) {
-      editPost(postID);
-    }
-
+ 
     // Delete Post:
     const deletePostBtn = target.dataset.deletepostbutton;
     if (deletePostBtn) {
@@ -142,11 +146,47 @@ export const PersonalFeed = () => {
       updateLikes(postID, currentUserEmail, valueToBeChanged, amountOfLikes, likeStatus);
     }
 
+    // Edit Post:
+    const editPostBtn = target.dataset.editpostbutton;
+    if (editPostBtn) {
+      const editTextArea = rootElement.querySelector(`[data-editTextArea='${postID}']`);
+      editTextArea.style.display = 'inline';
+      const saveEditBtn = rootElement.querySelector(`[data-saveEditPostButton='${postID}']`);
+      saveEditBtn.style.display = 'inline';
+      const contentTextPost = rootElement.querySelector(`[data-feedPostContent='${postID}']`);
+      contentTextPost.style.display = "none";
+
+    }
+    
+
+    const saveEditBtn = target.dataset.saveeditpostbutton;
+    if (saveEditBtn) {
+     
+      const newText = rootElement.querySelector(`[data-editTextArea='${postID}']`).value;
+      editPost(newText, postID);
+      loadPosts();
+    }
+
+    const cancelEditBtn = target.dataset.canceleditpostbutton;
+    if (cancelEditBtn) {
+      const editTextArea = rootElement.querySelector(`[data-editTextArea='${postID}']`);
+      editTextArea.style.display = 'none';
+      const hideSaveEditBtn = rootElement.querySelector(`[data-saveEditPostButton='${postID}']`);
+      hideSaveEditBtn.style.display = 'none';
+      const hideCancelEditBtn = rootElement.querySelector(`[data-cancelEditPostButton='${postID}']`);
+      hideCancelEditBtn.style.display = 'none';
+    }
+
     // Show Comments Section:
     const showCommentPostBtn = target.dataset.showcommentpostbutton;
     if (showCommentPostBtn) {
       const commentsSection = rootElement.querySelector(`[data-commentsSection="${postID}"]`);
-      commentsSection.style.display = 'flex';
+      if (commentsSection.style.display !== 'flex') {
+        commentsSection.style.display = 'flex';
+        getComments(postID, printComments);
+      } else {
+        commentsSection.style.display = 'none';
+      }
     }
 
     // Comment Post:
@@ -154,15 +194,66 @@ export const PersonalFeed = () => {
     if (commentPostBtn) {
       const newCommentContent = rootElement.querySelector(`[data-commentContent="${postID}"]`).value;
       getCurrentCommentsToPrint(postID, newCommentContent, currentUserEmail, printComments);
+      rootElement.querySelector(`[data-commentContent="${postID}"]`).value = '';
     }
+
+    // Delete Comment:
+    const deleteCommentBtn = target.dataset.deletecommentbutton;
+    if (deleteCommentBtn) {
+      const commentID = target.dataset.deletecommentbutton;
+      getCurrentCommentsToDelete(postIDForComments, commentID, printComments);
+    }
+
+    // Like Comment:
+    const likeCommentBtn = target.dataset.likecommentbutton;
+    if (likeCommentBtn) {
+      const commentID = target.dataset.likecommentbutton;
+      const valueToBeChanged = rootElement.querySelector(`[data-likeValueToChange="${commentID}"]`);
+      const amountOfLikes = parseInt(valueToBeChanged.textContent, 10);
+      const likeStatus = rootElement.querySelector(`[data-likeCommentButton="${commentID}"]`);
+      getCurrentCommentLikes(postIDForComments, currentUserEmail, commentID, valueToBeChanged,
+        amountOfLikes, likeStatus);
+    }
+
+
   });
 
   const printComments = (commentsToPrint, postID) => {
     const commentArea = rootElement.querySelector(`[data-ulCommentArea="${postID}"]`);
     commentArea.innerHTML = '';
     commentsToPrint.forEach((comment) => {
+      const currentDate = Math.round(Date.now() / 1000);
+      const timeInSeconds = (currentDate - comment.creationDate);
+      const commentAge = publicationAge(timeInSeconds);
       const newItem = `
-      <li class="comment-f-20" id="${comment.id}"> </li>
+      <li class='feed-comment-all-content' id="${comment.id}">
+      <hr class='feed-comment-start-line'>
+        <section class='feed-comment-owner-data'>
+          <img class='feed-comment-owner-picture' src='../images/bunny.jpg'>
+          <span class='feed-comment-owner-name'> ${comment.owner}</span>
+          <span class='feed-comment-data'> ${commentAge} </span>
+        </section>
+        <section class='feed-comment-content-section'>
+          <p class='feed-comment-content'> ${comment.content} </p>
+        </section>
+        <section class='feed-comments-actions-section'>
+      ${((likes) => {
+    if (likes.length > 0) {
+      if (likes.includes(currentUserEmail)) {
+        return `<button class='btn btn-from-comment full-like-comment-btn' data-likeCommentButton='${comment.id}'></button> `;
+      } return `<button class='btn btn-from-comment empty-like-comment-btn' data-likeCommentButton='${comment.id}'></button> `;
+    } return `<button class='btn btn-from-comment empty-like-comment-btn' data-likeCommentButton='${comment.id}'></button> `;
+  })(comment.likes)}
+        <span class='feed-comment-amount-of-likes' data-likeValueToChange='${comment.id}'> ${comment.likes.length} </span>
+      ${((user) => {
+    if (user === currentUserEmail) {
+      return `<button class='btn btn-from-comment delete-comment-btn' data-deleteCommentButton='${comment.id}'></button>`;
+    } return `<button class='not-allowed-to-see'></button>
+            `;
+  })(comment.owner)}
+      </section>
+       
+      </li>
       `;
       commentArea.innerHTML += newItem;
     });
@@ -200,12 +291,6 @@ export const PersonalFeed = () => {
     target.style.opacity = '0';
   }
 
-  function editPost(postID) {
-    const paragrafoEditar = document.getElementById(postID).querySelector('p');
-    paragrafoEditar.contentEditable = true;
-    paragrafoEditar.focus();
-  }
-
   loadPosts();
   return rootElement;
-};
+ };
